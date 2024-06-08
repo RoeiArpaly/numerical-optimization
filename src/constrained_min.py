d11ca0b40dd55d7ebbd0153cb1d7fb39cdf9be42
RoeiArpaly <roei.arpaly@gmail.com>
2024-06-09 01:30:52 +0300

    initial commit

---
+++ src/constrained_min.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+from src.unconstrained_min import wolfe_conditions
+
+
+class InteriorPointMinimizer:
+    T = 1
+    MU = 10
+
+    def __init__(self):
+        self.success = False
+        self.x_path = []
+        self.f_path = []
+
+    def interior_pt(
+        self,
+        func,
+        ineq_constraints,
+        eq_constraints_mat,
+        eq_constraints_rhs,
+        x0,
+        tol=1e-8,
+        max_iter=100,
+
+        alpha=1.0,
+        c1=0.01,
+        c2=0.5,
+        wolfe_tol=1e-6,
+    ):
+        _alpha = alpha
+        eq_const_n = eq_constraints_mat.shape[0]
+        x = x0
+        t = self.T
+
+        f_x, g_x, h_x = func(x, True)
+        f_x_phi, g_x_phi, h_x_phi = phi(ineq_constraints, x)
+        f_x = t * f_x + f_x_phi
+        g_x = t * g_x + g_x_phi
+        h_x = t * h_x + h_x_phi
+
+        for _ in range(max_iter):
+            if eq_const_n:
+                block_matrix = np.concatenate([
+                    np.concatenate([h_x, eq_constraints_mat.T], axis=1),
+                    np.concatenate([eq_constraints_mat, np.zeros((eq_const_n, eq_const_n))], axis=1)
+                ], axis=0)
+            else:
+                block_matrix = h_x
+
+            eq_vec = np.concatenate([-g_x, np.zeros(block_matrix.shape[0] - g_x.shape[0])])
+
+            x_prev = np.inf
+            f_prev = np.inf
+            for i in range(max_iter):
+                p = np.linalg.solve(block_matrix, eq_vec)[: x.shape[0]]
+                lambda_ = np.matmul(p.transpose(), np.matmul(h_x, p)) ** 0.5
+                if 0.5 * (lambda_ ** 2) < tol or sum(abs(x_prev - x)) < tol or f_prev - f_x < tol:
+                    break
+
+                alpha = wolfe_conditions(f=func, x=x, p=p, alpha=_alpha, c=c1, t=c2, tol=wolfe_tol)
+
+                x_prev = x
+                f_prev = f_x
+                x = x + alpha * p
+                f_x, g_x, h_x = func(x, True)
+                f_x_phi, g_x_phi, h_x_phi = phi(ineq_constraints, x)
+                self.x_path.append(x)
+                self.f_path.append(f_x)
+                f_x = t * f_x + f_x_phi
+                g_x = t * g_x + g_x_phi
+                h_x = t * h_x + h_x_phi
+
+            if ineq_constraints.shape[0] / t < tol:
+                self.success = True
+                break
+            t *= self.MU
+
+        return x, func(x, True)[0], self.success
+
+
+def phi(ineq_constraints, x):
+    f_star = 0
+    g_star = 0
+    h_star = 0
+    for func in ineq_constraints:
+        f_x, g_x, h_x = func(x, True)
+        f_star += np.log(-f_x)
+        g = g_x / f_x
+        g_star += g
+        g_mesh = np.tile(
+            g.reshape(g.shape[0], -1), (1, g.shape[0])
+        ) * np.tile(g.reshape(g.shape[0], -1).T, (g.shape[0], 1))
+        h_star += (h_x * f_x - g_mesh) / f_x**2
+    return -f_star, -g_star, -h_star
---
+++ tests/test_constrained_min.py
@@ -0,0 +1,87 @@
+import numpy as np
+import unittest
+
+from examples import (
+    test_qp,
+    test_qp_ineq_constraint_1,
+    test_qp_ineq_constraint_2,
+    test_qp_ineq_constraint_3,
+    test_lp,
+    test_lp_ineq_constraint_1,
+    test_lp_ineq_constraint_2,
+    test_lp_ineq_constraint_3,
+    test_lp_ineq_constraint_4,
+)
+from src.constrained_min import InteriorPointMinimizer
+from src.utils import (
+    plot_feasible_region_2d,
+    plot_feasible_region_3d,
+    plot_iterations,
+)
+
+
+class TestUnconstrainedMin(unittest.TestCase):
+
+    TEST_FUNCTIONS = [test_qp, test_lp]
+    OBJ_TOL = 1e-8
+    MAX_ITER = 100
+
+    def test_constrained_minimization(self):
+
+        for func in self.TEST_FUNCTIONS:
+            print(f"Testing function: {func.__name__}")
+
+            if func == test_qp:
+                x0 = np.array([0.1, 0.2, 0.7])
+                ineq_constraints = np.array([
+                    test_qp_ineq_constraint_1,
+                    test_qp_ineq_constraint_2,
+                    test_qp_ineq_constraint_3,
+                ])
+                A = np.array([[1, 1, 1]])
+                b = np.array([1])
+                plot_func = plot_feasible_region_3d
+            elif func == test_lp:
+                x0 = np.array([0.5, 0.75])
+                ineq_constraints = np.array([
+                    test_lp_ineq_constraint_1,
+                    test_lp_ineq_constraint_2,
+                    test_lp_ineq_constraint_3,
+                    test_lp_ineq_constraint_4,
+                ])
+                A = np.array([])
+                b = np.array([])
+                plot_func = plot_feasible_region_2d
+            else:
+                raise ValueError("Invalid function")
+
+            # Solve the problem
+            minimizer = InteriorPointMinimizer()
+            x, f_x, success = minimizer.interior_pt(
+                func=func,
+                ineq_constraints=ineq_constraints,
+                eq_constraints_mat=A,
+                eq_constraints_rhs=b,
+                x0=x0,
+                tol=self.OBJ_TOL,
+                max_iter=self.MAX_ITER,
+            )
+            # last iteration details:
+            print(f"(x, y): {x.round(3)}, f(x, y): {round(f_x, 3)}, success: {success}")
+
+            shape_name = func.__name__.removeprefix("test_").replace("_", " ").title()
+            plot_func(
+                f=func,
+                title=f"{shape_name.upper()}: Contour lines",
+                paths=np.array([minimizer.x_path]),
+                names=["interior_pt"],
+            )
+            plot_iterations(
+                title=f"{shape_name.upper()}: Function values vs. iteration number",
+                f_values=np.array([minimizer.f_path]),
+                names=["interior_pt"],
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
